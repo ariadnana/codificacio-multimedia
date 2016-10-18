127de6cf2dd6f22932e9e1d14ef6a3eb6bec0778
ariadnana <ariadna.xicota@estudiant.upc.edu>
2016-10-18 19:40:26 +0200

    canvis practica 5 18/10

--- Xicota_Ariadna_p05/app.js
+++ Xicota_Ariadna_p05/app.js
@@ -1,11 +1,29 @@
 
 var express = require('express');
+var Item = require('./Item.js').Item;
 var app = express();
 
-app.use(express.static('./'));
+// Configure jade to be our rendering engine
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
 
+
+// Enable boostrap from npm as a served static directory
+app.use("/libs",express.static('node_modules/bootstrap/dist'));
+
+// Our CSS and JS files
+app.use("/public",express.static('public'));
+
+//articles
+var pics={
+
+}
+
+// Render frontpage
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.render('base',{
+	pics:pics
+    });
 });
 
 app.listen(3000, function () {
